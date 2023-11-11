3482ed62ba8078899318b63dba8f7faaf1aa6431
金钊 <2427957775@qq.com>
2023-11-11 16:30:57 +0800

    完成无折扣接口，通过测试return_100_when_given_100

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -3,6 +3,16 @@
 namespace PriceCal{
     double PriceCalculator::AcceptPrice(const double price, const DiscountType type) const noexcept
     {
+        double finalPrice;
+        switch (type)
+        {
+        case DiscountType::NONE_DISCOUNT:
+            finalPrice = price;
+            return finalPrice;
+            break;
+        default:
+            break;
+        }
         return 0;
     }
 }
