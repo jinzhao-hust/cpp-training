0f10df80289f7ca07d8f941ec8bdf102c7e0fc48
金钊 <2427957775@qq.com>
2023-11-11 16:28:49 +0800

    再次修复无折扣测试接口1

--- tests/PrcieCalculatorTest.cpp
+++ tests/PrcieCalculatorTest.cpp
@@ -7,8 +7,21 @@ using namespace PriceCal;
 
 TEST(PriceCalculator, return_100_when_given_100)
 {
+    // given
     PriceCalculator priceCalculator;
-    priceCalculator.AcceptPrice(100, DiscountType::NONE_DISCOUNT);
-    EXPECT_DOUBLE_EQ(100, test());
+
+    // when
+    double cash = priceCalculator.AcceptPrice(100, DiscountType::NONE_DISCOUNT);
+    
+    // then
+    EXPECT_DOUBLE_EQ(100, cash);
 }
+
+// TEST(PriceCalculator, return_81_when_given_90)
+// {
+//     PriceCalculator priceCalculator;
+//     priceCalculator.AcceptPrice(90, DiscountType::PERCENT_DISCOUNT);
+//     EXPECT_DOUBLE_EQ(81, test());
+// }
+
 
