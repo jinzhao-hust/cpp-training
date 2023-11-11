2be5a17403f07acf25e558270bde349865a8f4c3
金钊 <2427957775@qq.com>
2023-11-11 16:38:24 +0800

    通过返现折扣测试return_90_when_given_90

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -4,6 +4,8 @@ namespace PriceCal{
     double PriceCalculator::AcceptPrice(const double price, const DiscountType type) const noexcept
     {
         double finalPrice;
+        int cashBackCount = 0;
+        int cashBackNum = 20;
         switch (type)
         {
         case DiscountType::NONE_DISCOUNT:
@@ -12,6 +14,10 @@ namespace PriceCal{
             break;
         case DiscountType::PERCENT_DISCOUNT:
             finalPrice = price * 0.9;
+            return finalPrice;
+        case DiscountType::CASH_DISCOUNT:
+            cashBackCount = price / 100;
+            finalPrice = price - cashBackCount * cashBackNum;
             return finalPrice;
         default:
             break;
