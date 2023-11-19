8971a8614ad2afc1e05016d3ff577a961dfe1b86
金钊 <2427957775@qq.com>
2023-11-19 13:43:29 +0800

    将三种折扣类修正为PriceCalculator的子类

--- include/PriceCalculator.h
+++ include/PriceCalculator.h
@@ -16,26 +16,27 @@ namespace PriceCalc
     public:
         
         double AcceptCash(const DiscountType discountType, const double money) const noexcept;
-    };
-    class Normal final
-    {
-    public:
-        double AcceptCash(const double money) const noexcept{
-            return money;
-        }
-    };
-    class PercentOff final
-    {
-    public:
-        double AcceptCash(const double money) const noexcept{
-            return money * 0.9;
-        }
-    };
-    class CashBack final
-    {
-    public:
-        double AcceptCash(const double money) const noexcept{
-            return money - ((int)(money/100)) * 20;
-        }
+    private:
+        class Normal final
+        {
+        public:
+            double AcceptCash(const double money) const noexcept{
+                return money;
+            }
+        };
+        class PercentOff final
+        {
+        public:
+            double AcceptCash(const double money) const noexcept{
+                return money * 0.9;
+            }
+        };
+        class CashBack final
+        {
+        public:
+            double AcceptCash(const double money) const noexcept{
+                return money - ((int)(money/100)) * 20;
+            }
+        };
     };
 } // namespace PriceCalc
