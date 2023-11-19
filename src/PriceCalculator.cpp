3b150ddadd506dfcb024033c32d51257a2f4e712
金钊 <2427957775@qq.com>
2023-11-19 14:30:37 +0800

    使用表驱动实现AcceptCash接口

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -3,39 +3,47 @@
 #include <cmath>
 #include <iostream>
 #include <memory>
+#include <unordered_map>
 namespace PriceCalc
 {
     double PriceCalculator::AcceptCash(const DiscountType discountType, const double money) const noexcept
     {
         double cash = 0.0;
-        std::unique_ptr<Discount> discount;
-        switch (discountType)
-        {
-        case DiscountType::CASH_NORMAL:
-            discount = std::make_unique<Normal>();
-            break;
+        std::unordered_map<DiscountType, std::unique_ptr<Discount>> discountMap;
+        discountMap.emplace(DiscountType::CASH_NORMAL,std::make_unique<Normal>());
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, std::make_unique<PercentOff>(0.9));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_20, std::make_unique<PercentOff>(0.8));
+        discountMap.emplace(DiscountType::CASH_PERCENTOFF_30, std::make_unique<PercentOff>(0.7));
+        discountMap.emplace(DiscountType::CASH_BACK, std::make_unique<CashBack>(100.0, 20.0));
+        return discountMap.find(discountType)->second->AcceptCash(money);
+        // std::unique_ptr<Discount> discount;
+        // switch (discountType)
+        // {
+        // case DiscountType::CASH_NORMAL:
+        //     discount = std::make_unique<Normal>();
+        //     break;
 
-        case DiscountType::CASH_PERCENTOFF_10:
-            discount = std::make_unique<PercentOff>(0.9);
-            break;
+        // case DiscountType::CASH_PERCENTOFF_10:
+        //     discount = std::make_unique<PercentOff>(0.9);
+        //     break;
 
-        case DiscountType::CASH_PERCENTOFF_20:
-            discount = std::make_unique<PercentOff>(0.8);
-            break;
+        // case DiscountType::CASH_PERCENTOFF_20:
+        //     discount = std::make_unique<PercentOff>(0.8);
+        //     break;
 
-        case DiscountType::CASH_PERCENTOFF_30:
-            discount = std::make_unique<PercentOff>(0.7);
-            break;
+        // case DiscountType::CASH_PERCENTOFF_30:
+        //     discount = std::make_unique<PercentOff>(0.7);
+        //     break;
 
-        case DiscountType::CASH_BACK:
-            discount= std::make_unique<CashBack>(100.0,20.0);
-            break;
+        // case DiscountType::CASH_BACK:
+        //     discount= std::make_unique<CashBack>(100.0,20.0);
+        //     break;
 
-        default:
-            break;
-        }
-        cash = discount->AcceptCash(money);
-        return cash;
+        // default:
+        //     break;
+        // }
+        // cash = discount->AcceptCash(money);
+        // return cash;
     }
     
 } // namespace PriceCalc
