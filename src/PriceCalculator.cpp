fb1b0b99fdf6c53ddf860c0c94a7aafd431662aa
金钊 <2427957775@qq.com>
2023-11-28 19:25:24 +0800

    在AcceptCash函数添加打印PriceCalculator实例地址的语句

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -10,6 +10,7 @@ namespace PriceCalc
     {
         // TODO 修改为map在整个声明周期只生成一次，也就是在这个test期间只生成一次，而PriceCalculator则可能生成多次
         // 可以使用static，也可以使用工厂模式的单体类实现。
+        std::cout<<this<<std::endl;
         std::unordered_map<DiscountType, std::unique_ptr<Discount>> discountMap;
         discountMap.emplace(DiscountType::CASH_NORMAL,std::make_unique<Normal>());
         discountMap.emplace(DiscountType::CASH_PERCENTOFF_10, std::make_unique<PercentOff>(0.9));
