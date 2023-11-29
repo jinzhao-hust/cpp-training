c6c4de180e7ea63aa69897df6f5bf3e4c0b1ad4c
金钊 <2427957775@qq.com>
2023-11-29 15:48:12 +0800

    修复测试接口为传引用，并通过测试

--- tests/PrcieCalculatorTest.cpp
+++ tests/PrcieCalculatorTest.cpp
@@ -7,7 +7,7 @@ using namespace PriceCalc;
 TEST(PriceCalculator, should_return_100_when_given_cash_normal_and_price_100)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_NORMAL;
     double money = 100.0;
 
@@ -21,7 +21,7 @@ TEST(PriceCalculator, should_return_100_when_given_cash_normal_and_price_100)
 TEST(PriceCalculator, should_return_90_when_cash_percent_off_10_and_price_100)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_PERCENTOFF_10;
     double money = 100.0;
 
@@ -35,7 +35,7 @@ TEST(PriceCalculator, should_return_90_when_cash_percent_off_10_and_price_100)
 TEST(PriceCalculator, should_return_80_when_cash_percent_off_20_and_price_100)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_PERCENTOFF_20;
     double money = 100.0;
 
@@ -49,7 +49,7 @@ TEST(PriceCalculator, should_return_80_when_cash_percent_off_20_and_price_100)
 TEST(PriceCalculator, should_return_70_when_cash_percent_off_30_and_price_100)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_PERCENTOFF_30;
     double money = 100.0;
 
@@ -63,7 +63,7 @@ TEST(PriceCalculator, should_return_70_when_cash_percent_off_30_and_price_100)
 TEST(PriceCalculator, should_return_90_when_given_cash_back_and_price_90)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_BACK;
     double money = 90.0;
 
@@ -77,7 +77,7 @@ TEST(PriceCalculator, should_return_90_when_given_cash_back_and_price_90)
 TEST(PriceCalculator, should_return_80_when_given_cash_back_and_price_100)
 {
     // given
-    PriceCalculator priceCalculator = PriceCalculator::GetInstance();
+    PriceCalculator& priceCalculator = PriceCalculator::GetInstance();
     DiscountType discountType = DiscountType::CASH_BACK;
     double money = 100.0;
 
